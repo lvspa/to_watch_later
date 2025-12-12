1166529131e1462a3225229198a8f26d8fecc01b
Alexandre <alexandrevps22@proton.me>
2025-12-12 20:02:22 -0300

    feat: models editados

--- to_watch_later/admin.py
+++ to_watch_later/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from to_watch_later.models import User
+from to_watch_later.models import Movie
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Movie)
--- to_watch_later/models.py
+++ to_watch_later/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Movie (models.Model):
-    name=models.CharField(max_length=100)
+    name_movie=models.CharField(max_length=100)
     gen=models.CharField(max_length=100)
     duration=models.IntegerField()
-    data_add=models.DateTimeField(auto_now_add=True)
+    #data_add=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.name_movie
 
     def __str__(self):
         return self.gen
@@ -18,12 +18,12 @@ class Movie (models.Model):
 
 
 class User (models.Model):
-    name=models.CharField(max_length=100)
+    name_user=models.CharField(max_length=100)
     email=models.EmailField(max_length=300)
     password=models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return self.name_user
 
     def __str__(self):
         return self.email
