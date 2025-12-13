16f210668c1e403933ebea775470b3d22df4a6f9
Alexandre <alexandrevps22@proton.me>
2025-12-13 12:20:12 -0300

    feat: classe intermediaria entre user e movie criada

--- to_watch_later/models.py
+++ to_watch_later/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.db.models import options
 
-
-class Movie (models.Model):
-    name_movie=models.CharField(max_length=100)
-    gen=models.CharField(max_length=100)
-    duration=models.IntegerField()
-    poster=models.ImageField()
-    #data_add=models.DateTimeField(auto_now_add=True)
+class Movie(models.Model):
+    name_movie = models.CharField(max_length=100)
+    gen = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    # poster=models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
+    date_add = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name_movie
@@ -17,8 +17,11 @@ class Movie (models.Model):
     def __int__(self):
         return self.duration
 
+    #def ____(self):
+        return self.date_add
 
 class User (models.Model):
+
     name_user=models.CharField(max_length=100)
     email=models.EmailField(max_length=300)
     password=models.IntegerField()
@@ -29,8 +32,21 @@ class User (models.Model):
     def __str__(self):
         return self.email
 
-class RelationUserMovie():
-    
+    def __str__(self):
+        return self.movies
+
+
+class ListMovies(models.Model):
+    movies= models.ForeignKey(Movie, on_delete=models.CASCADE)
+    users= models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
 
 
 
