0567b3d0cdf0c841e36b41d9baeaa0f49dc9fe7d
Alexandre <alexandrevps22@proton.me>
2025-12-13 19:26:40 -0300

    feat: classes ajustadas

--- to_watch_later/admin.py
+++ to_watch_later/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from to_watch_later.models import User
 from to_watch_later.models import Movie
+from to_watch_later.models import ListMovies
 
 admin.site.register(User)
 admin.site.register(Movie)
+admin.site.register(ListMovies)
--- to_watch_later/models.py
+++ to_watch_later/models.py
@@ -6,13 +6,10 @@ class Movie(models.Model):
     gen = models.CharField(max_length=100)
     duration = models.IntegerField()
     # poster=models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
-    date_add = models.DateTimeField(auto_now_add=True)
+    #date_add = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name_movie
-
-    def __str__(self):
-        return self.gen
 
     def __int__(self):
         return self.duration
@@ -24,21 +21,19 @@ class User (models.Model):
 
     name_user=models.CharField(max_length=100)
     email=models.EmailField(max_length=300)
-    password=models.IntegerField()
+    password=models.CharField(max_length=1000)
 
     def __str__(self):
         return self.name_user
-
-    def __str__(self):
-        return self.email
-
-    def __str__(self):
-        return self.movies
 
 
 class ListMovies(models.Model):
     movies= models.ForeignKey(Movie, on_delete=models.CASCADE)
     users= models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural='Movies List'
+
 
 
 
