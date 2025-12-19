02bb53acb7c83b0bbb686fe01e8daed324265fa5
Alexandre <alexandrevps22@proton.me>
2025-12-19 19:00:25 -0300

    feat: rotas melhoradas

--- to_watch_later/views.py
+++ to_watch_later/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-import os
-
-# Create your views here.
 
 def index(request):
-    #path_ext=os.path.abspath('/PycharmProjects/DjangoProject/ToWatchLater/to_watch_later/templates/to_watch_later/index.html')
     return render(request,'/home/Alexandre/PycharmProjects/DjangoProject/ToWatchLater/templates/index.html')
+
