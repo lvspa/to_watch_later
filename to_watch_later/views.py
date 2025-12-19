b0ef86ef37fbf300efb3f189ae7a51ac9e76f8d1
Alexandre <alexandrevps22@proton.me>
2025-12-19 15:03:07 -0300

    feat: rota para html concluida

--- to_watch_later/views.py
+++ to_watch_later/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
+import os
 
 # Create your views here.
 
 def index(request):
-
-
-
-
-    return render(request,'to_watch_later/front/index.html')
+    #path_ext=os.path.abspath('/PycharmProjects/DjangoProject/ToWatchLater/to_watch_later/templates/to_watch_later/index.html')
+    return render(request,'/home/Alexandre/PycharmProjects/DjangoProject/ToWatchLater/templates/index.html')
