cf68de3b886d3921945f612d6ce3dea9318a239a
Alexandre <alexandrevps22@proton.me>
2025-12-21 15:11:07 -0300

    feat: inclusao de caminho para static e criação dos arquivos css

--- to_watch_later/urls.py
+++ to_watch_later/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from. import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
+    #para cada pagina uma rota é necessaria, determinamos a rota em path, a function pra acesso em view
+    # e no html o {% url 'cads' %} para acesso
     path('', views.index,name='index'),
+    path('templates/cads.html', views.cads,name='cads'),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
--- to_watch_later/views.py
+++ to_watch_later/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request,'/home/Alexandre/PycharmProjects/DjangoProject/ToWatchLater/templates/index.html')
 
+def cads(request):
+    return render(request,'/home/Alexandre/PycharmProjects/DjangoProject/ToWatchLater/templates/cads.html')
